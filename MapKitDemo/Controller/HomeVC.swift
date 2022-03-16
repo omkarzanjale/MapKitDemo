a35f7ba1456a032f7e2e3fe961bcc775654cd476
Omkar <90714382+omkarzanjale@users.noreply.github.com>
2022-03-16 10:59:43 +0530

    UI Update & current Loc bug fixes

--- MapKitDemo/Controller/HomeVC.swift
+++ MapKitDemo/Controller/HomeVC.swift
@@ -12,21 +12,41 @@ import CoreLocation
 class HomeVC: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var addressView: UIView!
     lazy var locationManager = CLLocationManager()
-
+    lazy var currentLocation = CLLocationCoordinate2D()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureAction))
-        self.mapView.addGestureRecognizer(tapGesture)
+        config()
     }
     
-    @IBAction func currentLocBtnAction(_ sender: Any) {
+    private func config() {
+        self.addressView.isHidden = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureAction))
+        self.mapView.addGestureRecognizer(tapGesture)
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         if (CLLocationManager.locationServicesEnabled())
         {
             locationManager.startUpdatingLocation()
+        }
+    }
+    
+    private func removePreviousPin() {
+        if mapView.annotations.isEmpty == false {
+            for annotation in mapView.annotations {
+                self.mapView.removeAnnotation(annotation)
+            }
+        }
+    }
+    
+    @IBAction func currentLocBtnAction(_ sender: Any) {
+        removePreviousPin()
+        self.getAddress(coordnates: currentLocation) { address in
+            self.setPinUsingMKPointAnnotation(location: self.currentLocation, address: address)
         }
     }
     //
@@ -38,23 +58,20 @@ class HomeVC: UIViewController {
         geoCoder.reverseGeocodeLocation(location, completionHandler:
                                             { placemarks, error -> Void in
             guard let placeMark = placemarks?.first else { return }
-            //guard let locationName = placeMark.location  else {return}
-            //guard let street = placeMark.thoroughfare else {return}
             guard let city = placeMark.subAdministrativeArea  else {return}
             guard let zip = placeMark.isoCountryCode else {return}
             guard let country = placeMark.country  else {return}
             let address = "Placemark: \(placeMark.name ?? "No Name"), City: \(city), Country: \(country), Zip Code: \(zip)"
+            DispatchQueue.main.async {
+                self.lblAddress.text = address
+            }
             complisherHandler(address)
         })
     }
     
     @objc func tapGestureAction(sender: UIGestureRecognizer) {
         print("Tapped")
-        if mapView.annotations.isEmpty == false {
-            for annotation in mapView.annotations {
-                self.mapView.removeAnnotation(annotation)
-            }
-        }
+        removePreviousPin()
         let locFromTap = sender.location(in: mapView)
         let coordinatesOnMap = mapView.convert(locFromTap, toCoordinateFrom: mapView)
         self.getAddress(coordnates: coordinatesOnMap){ address in
@@ -84,10 +101,10 @@ extension HomeVC: MKMapViewDelegate {
 extension HomeVC: CLLocationManagerDelegate {
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         print("Region change")
-//        let northEast = mapView.convert(CGPoint(x: mapView.bounds.width, y: 0), toCoordinateFrom: mapView)
-//        let southWest = mapView.convert(CGPoint(x: 0, y: mapView.bounds.height), toCoordinateFrom: mapView)
-//        print(southWest)
-//        print(northEast)
+        //        let northEast = mapView.convert(CGPoint(x: mapView.bounds.width, y: 0), toCoordinateFrom: mapView)
+        //        let southWest = mapView.convert(CGPoint(x: 0, y: mapView.bounds.height), toCoordinateFrom: mapView)
+        //        print(southWest)
+        //        print(northEast)
     }
     //
     //MARK: Get current Location
@@ -95,11 +112,8 @@ extension HomeVC: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last{
             let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-            self.mapView.setRegion(region, animated: true)
-            self.getAddress(coordnates: center) { address in
-                self.setPinUsingMKPointAnnotation(location: center, address: address)
-            }
+            self.currentLocation = center
+            
         }
     }
 }
@@ -116,6 +130,7 @@ extension HomeVC {
     
     //Add pin using MKPointAnnotation
     func setPinUsingMKPointAnnotation(location: CLLocationCoordinate2D, address: String){
+        self.addressView.isHidden = false
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
         annotation.title = "Title"
@@ -123,9 +138,5 @@ extension HomeVC {
         let coordinateRegion = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
         mapView.setRegion(coordinateRegion, animated: true)
         mapView.addAnnotation(annotation)
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailsPopupVC")as? DetailsPopupVC {
-            vc.address = address
-            self.present(vc, animated: true, completion: nil)
-        }
     }
 }
